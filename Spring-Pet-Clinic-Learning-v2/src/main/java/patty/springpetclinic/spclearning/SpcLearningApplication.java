1f2c75b18d1d3a9696a809382fabdc45694a9187
Patrick Skelley <songkick.skelley@gmail.com>
2020-04-04 14:26:58 -0400

    initializing proj with intelliJ IDEA internal spring initializer

---
+++ Spring-Pet-Clinic-Learning-v2/src/main/java/patty/springpetclinic/spclearning/SpcLearningApplication.java
@@ -0,0 +1,13 @@
+package patty.springpetclinic.spclearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpcLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpcLearningApplication.class, args);
+    }
+
+}
---
+++ Spring-Pet-Clinic-Learning-v2/src/test/java/patty/springpetclinic/spclearning/SpcLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package patty.springpetclinic.spclearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpcLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
