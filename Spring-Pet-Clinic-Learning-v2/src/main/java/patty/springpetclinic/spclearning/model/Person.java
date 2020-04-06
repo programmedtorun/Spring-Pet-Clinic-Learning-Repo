be1bbc5f9a3690e4951aa9e20a50b7669949b5e6
Patrick Skelley <songkick.skelley@gmail.com>
2020-04-06 14:17:51 -0400

    initial model commit based on PetClinic git

---
+++ Spring-Pet-Clinic-Learning-v2/src/main/java/patty/springpetclinic/spclearning/model/Person.java
@@ -0,0 +1,31 @@
+package patty.springpetclinic.spclearning.model;
+
+/**
+ * Created by patrickskelley on Apr, 2020
+ */
+public class Person {
+
+    private String firstName;
+    private String lastName;
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
