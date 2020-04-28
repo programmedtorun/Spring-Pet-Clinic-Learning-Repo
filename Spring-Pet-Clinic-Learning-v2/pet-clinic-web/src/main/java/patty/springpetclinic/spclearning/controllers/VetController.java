24013212c40b1ae73596d3adce3ac270173b6c7a
Patrick Skelley <songkick.skelley@gmail.com>
2020-04-28 09:29:55 -0400

    adds listing of vets maps to vets/index.html. closes #19

--- Spring-Pet-Clinic-Learning-v2/pet-clinic-web/src/main/java/patty/springpetclinic/spclearning/controllers/VetController.java
+++ Spring-Pet-Clinic-Learning-v2/pet-clinic-web/src/main/java/patty/springpetclinic/spclearning/controllers/VetController.java
@@ -1,7 +1,9 @@
 package patty.springpetclinic.spclearning.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import patty.springpetclinic.spclearning.services.VetService;
 
 /**
  * Created by patrickskelley on Apr, 2020
@@ -10,7 +12,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String listVets(){ return "vets/index"; }
+    public String listVets(Model model){
+
+        model.addAttribute("vets", vetService.findAll());
+
+        return "vets/index";
+    }
 
 }
